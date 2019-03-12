35e14a1106fa3e470fd4f13b5bdb58ed15e0bac4
lidmyr93 <magnus.lidmyr@gmail.com>
2019-03-12 16:20:12 +0100

    js-fix

--- site/hamburger.js
+++ site/hamburger.js
@@ -11,15 +11,3 @@ function toggleMenu() {
 }
 /* Hamburger ends */
 
-/* controls for voting */
-const voting = document.querySelector('.show-vote');
-const voteknapp = document.querySelector('.vote-button');
-
-voteknapp.addEventListener('click', toggleVote);
-
-
-function toggleVote() {
-    voting.classList.toggle('vote');
-    voteknapp.innerHTML = (voteknapp.innerHTML == 'Rösta') ? '&#10761;' : 'Rösta';
-}
-/* controls for voting ends */
--- site/rating.js
+++ site/rating.js
@@ -1,3 +1,4 @@
+/* Prefix rating when page is loaded */
 const ratings = {
     filmOne : 4.2,
     filmTwo : 3.1,
@@ -7,39 +8,77 @@ const ratings = {
     filmSix : 1.2,
     filmSeven : 3.2,
     filmEigth : 2.1,
+    oldFilmOne : 4.4,
+    oldFilmTwo : 4.1,
+    oldFilmThree : 3.5,
+    oldFilmFour : 2.5,
+    oldFilmFive : 2.0,
+    oldFilmSix: 1.2,
+    oldFilmSeven : 4.3,
+    oldFilmEigth : 4.9,
 };
 
-const starsTotal = 5;
-// Kör getRatings när DOM:en har laddat
-document.addEventListener('DOMContentLoaded',getRatings);
 
+const starsTotal = 5; /* Used to calculate procent of the stars that you get from user */
+
+
+document.addEventListener('DOMContentLoaded', getRatings); /* To get the prefixed rating when DOM is loaded */
+
+
+/* Getting the control */
 const productSelect = document.getElementById('film-select');
 const ratingControl = document.getElementById('rating-control');
+
+const oldproductSelect = document.getElementById('oldfilm-select');
+const oldratingControl = document.getElementById('oldrating-control');
 
 let film;
 
 // Film select change
 productSelect.addEventListener('change', (e) =>{
+    console.log(e);
     film = e.target.value;
     ratingControl.disabled = false;
     ratingControl.value = ratings[film];
-})
+});
+oldproductSelect.addEventListener('change', (e) =>{
+    film = e.target.value;
+    oldratingControl.disabled = false;
+    oldratingControl.value = ratings[film];
+});
 // rating kontroll
 
-ratingControl.addEventListener('blur', (e) =>{
+ratingControl.addEventListener('click', (e) =>{
     const rating = e.target.value;
-    
+    console.log(rating);
     if(rating > 5){
         alert('Rösta bara mellan 1 och 5')
         return;
     }
     ratings[film] = rating;
     getRatings();
-    console.log(ratings); // här kanske spara objectet rating till localstorage.
+    
 });
+
+
+oldratingControl.addEventListener('click', (e) =>{
+    const rating = e.target.value;
+    console.log(rating);
+    if(rating > 5){
+        alert('Rösta bara mellan 1 och 5')
+        return;
+    }
+    ratings[film] = rating;
+    getRatings();
+    
+});
+
+
+
 
 // Hämtar ratings
 function getRatings(){
+   
     for(let rating in ratings){
         // ratings till procent
         const starPercentage = (ratings[rating] / starsTotal) * 100;
@@ -52,3 +91,6 @@ function getRatings(){
         document.querySelector(`.${rating} .number-rating`).innerHTML = ratings[rating];
     }
 }
+
+
+ 
